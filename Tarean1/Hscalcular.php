bb287cedec8553d18f9ad495b763cade75ae73f2
Oswaldo Gamboa <84478962+Oswald-prime@users.noreply.github.com>
2021-10-17 15:22:04 -0400

    Add files via upload

---
+++ Tarean1/Hscalcular.php
@@ -0,0 +1,19 @@
+<?php 
+
+$catetouno = 3;
+$Catetodos = 4;
+
+$catetounoc = pow($catetouno,2);
+$catetodosc = pow($Catetodos,2);
+
+
+$hipo = sqrt($catetounoc+$catetodosc);
+
+    echo "el Lado A es de:".$catetouno;
+    echo "<br>";
+    echo "el lado B es de:".$Catetodos;
+    echo "<br>";
+    echo "la hipotenusa es de:".$hipo;
+
+?>
+
